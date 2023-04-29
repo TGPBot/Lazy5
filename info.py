ef7db2bf0b92550782c242a2b02587dc51af1b39
TGPBot <111167531+TGPBot@users.noreply.github.com>
2023-04-29 15:59:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/3aecb4f98d11d57d7aa07.jpg h
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '978408962').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001877173739').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001967676586').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '978408962').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL', '-1001600751799')
